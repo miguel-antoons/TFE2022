5676dc4e8ceeb03a6d46f4340bc1595bc1a70bc8
miguel-antoons <miguel.antoons@gmail.com>
2022-02-24 17:49:30 +0100

    showing spectrogram altough the spectrogram is not precise enough

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 from scipy import signal
-from scipy.fft import fftshift
+from scipy.io import wavfile
+import numpy as np
 import matplotlib.pyplot as plt
-import wave
 
 
 def main():
-    print("Loading wav file into memory")
-    recording_info = wave.open(
-        './recordings/RAD_BEDOUR_20220211_1730_BEDINA_SYS001.wav',
-        'r'
+    print("Loading wav file into memory...")
+    sample_frequency, audio_signal = wavfile.read(
+        './recordings/RAD_BEDOUR_20220211_1730_BEDINA_SYS001.wav'
+    )
+    print(f'Sample frequency : {sample_frequency}')
+    print(f'Signal length : {len(audio_signal)}')
+
+    print('Calculating spectrogram...')
+    frequencies, times, spectrogram = signal.spectrogram(
+        audio_signal, sample_frequency, nperseg=16384
     )
 
-    sound_signal = recording_info.readframes(500)
-    print(recording_info.getparams())
-    print(sound_signal)
+    print('Filtering unneeded frequencies...')
+    fmin = 1000
+    fmax = 1200
+    freq_slice = np.where((frequencies >= fmin) & (frequencies <= fmax))
+    frequencies = frequencies[freq_slice]
+    spectrogram = spectrogram[freq_slice, :][0]
 
-    return
+    print(f'Sample frequencies : {frequencies}')
+    print(f'Segment times : {times}')
+
+    print('Preparing and showing graph...')
+    plt.pcolormesh(times, frequencies, spectrogram, shading='auto', norm=1.0)
+    # plt.imshow(spectrogram)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
 
 
 if __name__ == '__main__':
