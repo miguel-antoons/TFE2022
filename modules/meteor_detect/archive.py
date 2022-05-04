0d608fd00aa662e1d1a114f9ce9a746880a1037e
miguel-antoons <miguel.antoons@gmail.com>
2022-05-04 12:11:14 +0200

    adding compatibility with python3.6

--- modules/meteor_detect/archive.py
+++ modules/meteor_detect/archive.py
@@ -6,19 +6,22 @@ from datetime import datetime
 def verify_archive_date(search_date, parent_directory):
     dir_content = os.listdir(parent_directory)
 
-    if (year := search_date.strftime('%Y')) not in dir_content:
+    year = search_date.strftime('%Y')
+    if year not in dir_content:
         return False
 
     directory = os.path.join(parent_directory, year)
     dir_content = os.listdir(directory)
 
-    if (month := search_date.strftime('%m')) not in dir_content:
+    month = search_date.strftime('%m')
+    if month not in dir_content:
         return False
 
     directory = os.path.join(directory, month)
     dir_content = os.listdir(directory)
 
-    if (day := search_date.strftime('%d')) not in dir_content:
+    day = search_date.strftime('%d')
+    if day not in dir_content:
         return False
 
     return {
@@ -45,17 +48,14 @@ def get_archived_files(requested_files, precise_time, parent_directory):
             os.path.isfile(file_path)
             and split_filename[4] in requested_files.keys()
         ):
-            if (
-                antenna := str(int(
-                    split_filename[5]
-                    .replace('SYS', '')
-                    .replace('.wav', '')
-                ))
-            ) in requested_files[split_filename[4]].keys():
-                if (
-                    (date := file_date.strftime('%Y%m%d%H%M'))
-                    in requested_files[split_filename[4]][antenna].keys()
-                ):
+            antenna = str(int(
+                split_filename[5]
+                .replace('SYS', '')
+                .replace('.wav', '')
+            ))
+            if antenna in requested_files[split_filename[4]].keys():
+                date = file_date.strftime('%Y%m%d%H%M')
+                if date in requested_files[split_filename[4]][antenna].keys():
                     print(requested_files[split_filename[4]][antenna][date])
                     requested_files[
                         split_filename[4]][antenna][date]["file_path"] = (
